f20e4c791b3b857514a6e63dc7327a3f1266bd1b
Thejasri <tjs9@Thejasris-MacBook-Air.local>
2022-04-06 09:36:11 +0530

    files modified

--- routes/index.js
+++ routes/index.js
@@ -1,84 +1,87 @@
-const express = require("express");
-const router = express.Router();
+const express = require('express')
+const router = express.Router()
 
-const { Employee } = require("../models/employee");
+const { Employee } = require('../models/employee')
 
-router.get("/api/employees", (_, res) => {
-  Employee.find({}, (err, data) => {
-    if (!err) {
-      res.send(data);
-    } else {
-      res.json(`Employee Details Not Found`);
-    }
-  });
-});
+router.get('/api/employees', (_, res) => {
+    Employee.find({}, (err, data) => {
+        if (!err) {
+            res.send(data)
+        } else {
+            res.json(`Employee Details Not Found`)
+        }
+    })
+})
 
 //post employee details
-router.post("/api/employee", (req, _) => {
-  const emp = new Employee({
-    name: req.body.name,
-    id: req.body.id,
-    salary: req.body.salary,
-    email: req.body.email,
-  });
-
-  emp.save((res, data) => {
+router.post('/api/employee', (req, _) => {
+    const emp = new Employee({
+        name: req.body.name,
+        id: req.body.id,
+        salary: req.body.salary,
+        email: req.body.email,
+    })
     try {
-      res
-        .statusCode(200)
-        .json({ message: "Employee Added Successfully", data: data });
+        emp.save((res, data) => {
+            res.statusCode(200).json({
+                message: 'Employee Added Successfully',
+                data: data,
+            })
+        })
     } catch (err) {
-      res.json(`Employee Data Not Added`);
+        res.json(`Employee Data Not Added`)
     }
-  });
-});
+})
 
 //get single employee
-router.get("/api/employee/:id", (req, res) => {
-  Employee.findById(req.params.id, (err, data) => {
-    if (!err) {
-      res
-        .status(200)
-        .json({ message: "Employee data fetched successfully", data: data });
-    } else {
-      res.json(`The Employee Not Found`);
-    }
-  });
-});
+router.get('/api/employee/:id', (req, res) => {
+    Employee.findById(req.params.id, (err, data) => {
+        if (!err) {
+            res.status(200).json({
+                message: 'Employee data fetched successfully',
+                data: data,
+            })
+        } else {
+            res.json(`The Employee Not Found`)
+        }
+    })
+})
 //update employee
-router.put("/api/employee/:id", (req, res) => {
-  const emp = {
-    name: req.body.name,
-    id: req.body.id,
-    salary: req.body.salary,
-    email: req.body.email,
-  };
-  Employee.findByIdAndUpdate(
-    req.params.id,
-    { $set: emp },
-    { new: true },
-    (err, data) => {
-      if (!err) {
-        res
-          .status(200)
-          .json({ message: "Employee updated successfully", data: data });
-      } else {
-        res.json(`The Employee details not updated.`);
-      }
+router.put('/api/employee/:id', (req, res) => {
+    const emp = {
+        name: req.body.name,
+        id: req.body.id,
+        salary: req.body.salary,
+        email: req.body.email,
     }
-  );
-});
+    Employee.findByIdAndUpdate(
+        req.params.id,
+        { $set: emp },
+        { new: true },
+        (err, data) => {
+            if (!err) {
+                res.status(200).json({
+                    message: 'Employee updated successfully',
+                    data: data,
+                })
+            } else {
+                res.json(`The Employee details not updated.`)
+            }
+        }
+    )
+})
 //delete Employee
-router.delete("/api/employee/:id", (req, res) => {
-  Employee.findByIdAndDelete(req.params.id, (err, data) => {
-    if (!err) {
-      res
-        .status(200)
-        .json({ message: "Employee Deleted Successfully", data: data });
-    } else {
-      res.json(`The Employee Not Found`);
-    }
-  });
-});
+router.delete('/api/employee/:id', (req, res) => {
+    Employee.findByIdAndDelete(req.params.id, (err, data) => {
+        if (!err) {
+            res.status(200).json({
+                message: 'Employee Deleted Successfully',
+                data: data,
+            })
+        } else {
+            res.json(`The Employee Not Found`)
+        }
+    })
+})
 
-module.exports = router;
+module.exports = router

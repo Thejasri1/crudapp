7878cdbfffe30d54225d0911a90f451058e2c315
Thejasri <tjs9@Thejasris-MacBook-Air.local>
2022-04-06 11:07:15 +0530

    validations added

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-
+const { body } = require('express-validator')
 const { Employee } = require('../models/employee')
 
 router.get('/api/employees', (_, res) => {
@@ -13,23 +13,32 @@ router.get('/api/employees', (_, res) => {
     })
 })
 
+const isValidEmail = (email) => {
+    const re =
+        /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    return re.test(String(email).toLowerCase())
+}
+
 //post employee details
-router.post('/api/employee', (req, _) => {
-    const emp = new Employee({
-        name: req.body.name,
-        id: req.body.id,
-        salary: req.body.salary,
-        email: req.body.email,
-    })
-    try {
-        emp.save((res, data) => {
-            res.statusCode(200).json({
-                message: 'Employee Added Successfully',
-                data: data,
-            })
+router.post('/api/employee', (req, res) => {
+    if (isValidEmail(req.body.email)) {
+        const emp = new Employee({
+            name: req.body.name,
+            id: req.body.id,
+            salary: req.body.salary,
+            email: req.body.email,
         })
-    } catch (err) {
-        res.json(`Employee Data Not Added`)
+
+        try {
+            emp.save(emp).then((data) => {
+                res.send(data)
+                res.send('Emp Added')
+            })
+        } catch (err) {
+            res.json(`Employee Data Not Added`)
+        }
+    } else {
+        res.json('email is not valid')
     }
 })
 
@@ -48,27 +57,31 @@ router.get('/api/employee/:id', (req, res) => {
 })
 //update employee
 router.put('/api/employee/:id', (req, res) => {
-    const emp = {
-        name: req.body.name,
-        id: req.body.id,
-        salary: req.body.salary,
-        email: req.body.email,
-    }
-    Employee.findByIdAndUpdate(
-        req.params.id,
-        { $set: emp },
-        { new: true },
-        (err, data) => {
-            if (!err) {
-                res.status(200).json({
-                    message: 'Employee updated successfully',
-                    data: data,
-                })
-            } else {
-                res.json(`The Employee details not updated.`)
-            }
+    if (isValidEmail(req.body.email)) {
+        const emp = {
+            name: req.body.name,
+            id: req.body.id,
+            salary: req.body.salary,
+            email: req.body.email,
         }
-    )
+        Employee.findByIdAndUpdate(
+            req.params.id,
+            { $set: emp },
+            { new: true },
+            (err, data) => {
+                if (!err) {
+                    res.status(200).json({
+                        message: 'Employee updated successfully',
+                        data: data,
+                    })
+                } else {
+                    res.json(`The Employee details not updated.`)
+                }
+            }
+        )
+    } else {
+        res.json('Email is not valid!')
+    }
 })
 //delete Employee
 router.delete('/api/employee/:id', (req, res) => {
